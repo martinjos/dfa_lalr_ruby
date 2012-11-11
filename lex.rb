c36f91521fe055665b67bc5f1f68e7fd4a4acd9b
Martincode <anonymous@example.com>
2012-11-11 19:41:48 +0000

    Virtual rewrite of the lexer to get it to work correctly. Fortunately it's quite simple now.

--- lex.rb
+++ lex.rb
@@ -1,36 +1,74 @@
 require 'set'
 
 class LexError < StandardError
+    def initialize(pos, len)
+        if pos == len
+            super("at EOF")
+        else
+            super("at position #{pos}")
+        end
+    end
 end
 
-def lex(dfa, enum)
+def lex(dfa, duck)
     start_state = Set.new( [0] )
     state = start_state
     str = ""
-    pos = 1
-    if !enum.respond_to?(:each) && enum.respond_to?(:chars)
-        enum = enum.chars
+    pos = 0
+    last_success = nil
+    last_str = nil
+    last_pos = nil
+
+    yield_success = proc do |su, s, p|
+        if su.size == 1
+            yield su.first, s, p
+        else
+            yield su, s, p
+        end
+        state = start_state
+        str = ""
+        last_success = nil # don't bother clearing last_str or last_pos
     end
-    # need to yield everything from enum, plus "" to finish off
-    # (transition always fails, no "" transitions can survive compilation)
+
+    # will need to yield everything from duck, possibly
+    # backtracking along the way, plus "" to finish off.
+    # (no "" transitions can survive compilation)
     p = proc do |ch|
+        puts "At position #{pos}"
         success = dfa[state].success
+        if !success.empty?
+            last_success = success
+            last_str = str
+            last_pos = pos
+        end
         next_state = dfa[state].lookup[ch]
         if next_state.nil?
-            if success.empty?
-                raise LexError, "position #{pos}"
-            elsif success.size == 1
-                yield success.first, str
+            if last_success.nil?
+                if str.empty? && pos == duck.size && state == start_state
+                    throw :done
+                else
+                    #puts "#{str} #{pos} #{duck.size} #{state.inspect} #{start_state.inspect}"
+                    raise LexError.new(pos, duck.size)
+                end
             else
-                yield success, str
+                yield_success.call last_success, last_str, last_pos
+                pos = last_pos
             end
-            str = ""
         else
             state = next_state
             str += ch
+            pos += 1
         end
-        pos += 1
     end
-    enum.each &p
-    p.call("")
+
+    size = duck.size
+    catch :done do
+        while true
+            if pos < size
+                p.call(duck[pos])
+            else
+                p.call("")
+            end
+        end
+    end
 end
