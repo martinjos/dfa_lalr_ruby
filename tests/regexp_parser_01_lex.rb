f7efd116cdc0201729f12225b7dc8a0413e497b7
Martincode <anonymous@example.com>
2012-11-15 16:14:33 +0000

    Make tests less verbose.

--- tests/regexp_parser_01_lex.rb
+++ tests/regexp_parser_01_lex.rb
@@ -2,5 +2,10 @@
 
 require 'tests/regexp_parser_common.rb'
 
-p = proc {|x,y,z| puts "#{x} - #{y} - #{z}"}
-lex(RegexpDFA, "123|foo\\||b(a)r\\**", &p)
+regexp = "123|foo\\||b(a)r\\**"
+expect_tokens = [[:char, "1", 1], [:char, "2", 2], [:char, "3", 3], ["|", "|", 4], [:char, "f", 5], [:char, "o", 6], [:char, "o", 7], [:char, "\\|", 9], ["|", "|", 10], [:char, "b", 11], ["(", "(", 12], [:char, "a", 13], [")", ")", 14], [:char, "r", 15], [:char, "\\*", 17], [:unary_op, "*", 18]]
+tokens = []
+p = proc {|x,y,z| tokens << [x,y,z]}
+lex(RegexpDFA, regexp, &p)
+assert(tokens == expect_tokens)
+#p tokens
